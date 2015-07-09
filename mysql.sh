dabc9791d68a34b7cd4db963530ac084d3bf72f7
Stan Rudenko <me@rooty.name>
2015-07-10 01:46:29 +0300

    initial commit scripts to public

---
+++ mysql.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+#
+# Backup of MySQL v.1.2
+# (c) Stanislav Rudenko, me [at] rooty [d0t] name
+#
+
+BASEDIR=$(dirname $0)
+
+. "${BASEDIR}/.config"
+
+NOW=$(/bin/date +"%d.%m.%Y.%H.%M")
+DBS="$($PATH_BIN_MYSQL --defaults-file=${MYSQL_DEF_FILE} -Bse 'show databases')"
+
+if [ -z "${DBS}" ]; then echo "!!!DATABASE LIST IS EMPTY!!!"; exit 1; fi;
+
+[ ! -d "${PATH_MYSQL_BACKUP}/${NOW}" ] && mkdir -p "${PATH_MYSQL_BACKUP}/${NOW}"
+
+echo '>>> Creating Backup of databases'
+
+for DB in ${DBS}
+ do
+
+  for DB_EXCEPTION in ${BACKUP_DB_EXCEPTIONS}
+   do
+    if [ "${DB}" = "${DB_EXCEPTION}" ]; then continue 2; fi;
+  done
+
+  echo "] Processing database \"${DB}\""
+
+  ${PATH_BIN_MYSQLDUMP} --defaults-file=${MYSQL_DEF_FILE} -f ${DB} > ${PATH_MYSQL_BACKUP}/${NOW}/mydb_backup_${DB}_database.sql
+
+done
+
+echo '>>> Cleaning old Backups'
+
+/usr/bin/find ${PATH_MYSQL_BACKUP} -ctime +${OLD_BACKUPS}w -type d -exec /bin/rm -rvf {} \;
+
+if [ ${COMPRESS_FILES} = "yes" ]
+ then
+
+  echo ">>> Creating an archive using ${PATH_BIN_ARCHIVE}"
+
+  for DB_FILE in $(ls -1 "${PATH_MYSQL_BACKUP}/${NOW}" | xargs)
+   do
+    
+    /usr/bin/nice -n 15 ${PATH_BIN_ARCHIVE} ${ARCHIVE_OPTIONS} "${PATH_MYSQL_BACKUP}/${NOW}/${DB_FILE}.7z" "${PATH_MYSQL_BACKUP}/${NOW}/${DB_FILE}" >/dev/null
+
+    if [ $? -eq 0 ]
+     then
+      rm -rf ${PATH_MYSQL_BACKUP}/${NOW}/${DB_FILE}
+     else
+      echo "!!!!! SOMETHING WRONG HAPPENS DUE COMPRESS FILE ${DB_FILE} !!!!!"
+    fi
+     
+  done
+
+fi
+
+echo ">>> Operation took time from ${NOW} to `/bin/date +"%d.%m.%Y.%H.%M"`"
+
+exit 0
---
+++ www.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+#
+# Backup of WebSites v.2.1
+# (c) Stanislav Rudenko, me [at] rooty [d0t] name
+#
+BASEDIR=$(dirname $0)
+
+. "${BASEDIR}/.config"
+. "${BASEDIR}/.www"
+
+NOW=$(/bin/date +"%d.%m.%Y.%H.%M")
+
+[ -n "${RUN_BEFORE_START}" ] && eval "${RUN_BEFORE_START}"
+
+for ACTIVE_PROJECT in ${ACTIVE_PROJECTS}
+ do
+
+  wd=$(eval echo "\${PATH_WEBSITE_ROOT_${ACTIVE_PROJECT}}")
+
+  if [ -z "$wd" ]
+  then
+    echo "I'll skip \"$wd\" because value is empty"
+    continue
+  fi
+
+  csn=$(echo ${ACTIVE_PROJECT} | tr '[:upper:]' '[:lower:]')
+
+  echo ">>> Processing (${csn}):"
+
+  if [ ! -d "$wd" ]
+   then
+    echo "I'll skip \"$wd\" due error... check this directory"
+    continue
+  fi
+
+  echo "${wd}"
+
+  sx=$(eval echo "\${PATH_WEBSITE_EXCLUDE_${ACTIVE_PROJECT}}")
+
+  exc_st=''
+
+  if [ ! -z "$sx" ]
+   then
+    for ST in $sx
+     do
+      exc_st="${exc_st}--exclude ${ST} "
+     done
+  fi
+
+  [ ! -d "${PATH_WWW_BACKUP}/${NOW}" ] && mkdir -p "${PATH_WWW_BACKUP}/${NOW}"
+
+  ESTART=$(/bin/date +"%s")
+
+  /usr/bin/nice -n 15 $PATH_BIN_TAR -cf - ${exc_st}-C ${wd} . | $PATH_BIN_ARCHIVE $ARCHIVE_OPTIONS -si ${PATH_WWW_BACKUP}/${NOW}/${csn}.7z >/dev/null
+
+  EEND=$(/bin/date +"%s")
+
+  echo "Elapsed time:" $(($EEND - $ESTART))
+
+done
+
+[ -n "${RUN_AT_THE_END}" ] && eval "${RUN_AT_THE_END}"
+
+echo '>>> Cleaning old Backups'
+
+/usr/bin/find ${PATH_WWW_BACKUP} -ctime +${OLD_BACKUPS}w -type d -exec /bin/rm -rvf {} \;
+
+echo ">>> Operation took time from ${NOW} to `/bin/date +"%d.%m.%Y.%H.%M"`"
+
+exit 0
